b9da3fbc07e1157cc3f3bb3d564d9cfb63f38df3
ColbyCabrera <gdthyispro@gmail.com>
2023-02-09 17:36:06 -0500

    Add new route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/cool", (req, res) => {
+    res.send("YOU'RE SO COOOL");
+})
+
 module.exports = router;
