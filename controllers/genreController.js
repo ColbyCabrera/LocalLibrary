5a4677250394ab99db27583e49d55fa78c953888
ColbyCabrera <gdthyispro@gmail.com>
2023-02-14 15:26:22 -0500

    Add views for genres

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,17 @@ const Genre = require("../models/genre");
 
 // Display list of all Genre.
 exports.genre_list = (req, res, next) => {
-  
+  Genre.find()
+    .sort({ name: 1 })
+    .exec(function (err, list_genres) {
+      if (err) {
+        return next(err);
+      }
+      res.render("genre_list", {
+        title: "Genre List",
+        genre_list: list_genres,
+      });
+    });
 };
 
 // Display detail page for a specific Genre.
